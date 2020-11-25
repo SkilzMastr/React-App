f9e0529ec0c74fc76141d4b9d5a127814318e93d
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-11-25 10:07:48 -0500

    Added Base

---
+++ styles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FF0000',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  });
