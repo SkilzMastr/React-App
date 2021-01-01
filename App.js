e97b5cd8af54e646c23b3db085e549d2912de21d
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-01 05:04:45 -0500

    Added Crap

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, Text, View } from 'react-native';
 import styles from './styles'
-
+var x = 0
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>{x}</Text>
       <Button 
         onPress={() => {
-          alert('Hello World')
+          x++
         }}
-        title="Press Here"
+        title="Increase"
       />
       <StatusBar style="auto" />
     </View>
--- styles.js
+++ styles.js
@@ -10,3 +10,5 @@ const styles = StyleSheet.create({
       justifyContent: 'center'
     },
   });
+
+export default styles;
