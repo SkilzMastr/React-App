4964156f4f8a3c4164b7d71ab2a786dafc5f449a
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-01 03:25:14 -0500

    Hopefully this works

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import styles from './styles'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
+      <Button 
+        onPress={() => {
+          alert('Hello World')
+        }}
+        title="Press Here"
+      />
       <StatusBar style="auto" />
     </View>
   );
