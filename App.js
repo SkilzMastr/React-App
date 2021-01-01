15f827fdb7ae2ffc72a768640a73adb1ee8045cf
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-01 05:16:00 -0500

    Did Stuff

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import styles from './styles'
 var x = 0
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>{x}</Text>
-      <Button 
-        onPress={() => {
-          x++
-        }}
-        title="Increase"
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <View style={styles.container}>
+          <Text>{x}</Text>
+          <Button 
+            onPress={() => {
+              x++
+            }}
+            title="Increase"
+          />
+          <StatusBar style="auto" />
+        </View>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
